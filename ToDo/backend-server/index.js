f159378c557b016a9c6745991f7ee17340d2b6f2
Kavindu Bimsara <34527634+kbimsara@users.noreply.github.com>
2025-01-10 23:09:36 +0530

    update:Server-side

--- ToDo/backend-server/index.js
+++ ToDo/backend-server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const retries = 5;
+let attempts = 0;
 const User = require('./models/user.model.js');
 const Task = require('./models/todo.model.js');
 
